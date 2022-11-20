1cec4b33e274d00d4d0649c4131e2f4e157efa26
CristinaLupashko <116310181+CristinaLupashko@users.noreply.github.com>
2022-11-20 16:55:01 +0100

    Add files via upload

---
+++ HomeWorkSecond.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class HomeWorkSecond {
+    public static void main(String[] args) {
+        Scanner scaner =new Scanner(System.in);
+        int number = scaner.nextInt();
+
+        while (number != 0) {
+            System.out.println("Я начинаю разбираться с циклами");
+            number --;
+        }
+        Scanner str = new Scanner(System.in);
+        String word = str.nextLine();
+        int len = word.length();
+        while (len != 0) {
+            System.out.println(word.charAt(0));
+            len --;
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
